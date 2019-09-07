4208a0b2247aa5d672ccc676a3659d727de19d6a
Harrison Chen <darkray@126.com>
2019-09-07 16:36:07 +0930

    [prep]react prep

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    mode: "development",
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
